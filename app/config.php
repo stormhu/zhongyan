d24460b1cac221c67f5024007efa6c1909f0b4b0
stormhu <stormhu@foxmail.com>
2017-04-26 14:23:36 +0800

    office

--- app/config.php
+++ app/config.php
@@ -139,10 +139,13 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+        '__STATIC__'            => '/public/static/' ,
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
+
 
     // +----------------------------------------------------------------------
     // | 异常及错误设置
